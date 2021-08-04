46b1a35ddc25826ec16e0a64a57b345c0fc6a87b
Valmir Massoni <valmirmassoni@gmail.com>
2021-08-04 19:13:43 -0300

    Add test case

---
+++ Example/Tests/ViewCodeCell.swift
@@ -0,0 +1,9 @@
+//
+//  ViewCodeCell.swift
+//  SelfTableViewManager_Tests
+//
+//  Created by Valmir Massoni on 04/08/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
